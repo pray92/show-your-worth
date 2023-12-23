eac61e061dc88930424f9e808776574f1c9bb37d
Jisu Choi <redgem92@gmail.com>
2023-12-23 16:43:55 +0900

    FEAT: local 전용 import.sql 추가

---
+++ src/main/resources-env/local/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(account_id, password, nickname, user_type, create_time) VALUES('account', 'password', 'nickname', 1, now());
+INSERT INTO users(account_id, password, nickname, user_type, create_time) VALUES('account2', 'password2', 'nickname2', 1, now());
+INSERT INTO users(account_id, password, nickname, user_type, create_time) VALUES('account3', 'password3', 'nickname3', 1, now());
+INSERT INTO users(account_id, password, nickname, user_type, create_time) VALUES('account4', 'password4', 'nickname4', 1, now());
+INSERT INTO users(account_id, password, nickname, user_type, create_time) VALUES('account5', 'password5', 'nickname5', 100, now());
