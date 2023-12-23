738373998fa9fdd3305cd3f110b01ebcf4d2358e
Jisu Choi <redgem92@gmail.com>
2023-12-23 15:13:20 +0900

    REFACTOR: 유저 컨트롤러 API 명칭 변경에 따른 테스트 케이스 수정

--- src/test/java/kr/texturized/muus/presentation/api/UserControllerTest.java
+++ src/test/java/kr/texturized/muus/presentation/api/UserControllerTest.java
@@ -21,76 +21,76 @@ public class UserControllerTest extends IntegrationTest {
 
     @Test
     void whenInvalidAccountIdThenReturnException() throws Exception {
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","asdfe", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","Asd12", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","asdfqwer12!", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","12!DSsaFfqwer", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","#####", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","1q2w3e4r1!", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","asdfqwerzxcvasdfqwer", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/account", "accountId","f-lab.kr", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","asdfe", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","Asd12", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","asdfqwer12!", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","12!DSsaFfqwer", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","#####", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","1q2w3e4r1!", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","asdfqwerzxcvasdfqwer", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-account", "accountId","f-lab.kr", ErrorCode.INVALID_INPUT_VALUE);
     }
 
     @Test
     void whenValidAccountIdThenReturnOK() throws Exception {
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "redgem92");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "12qewrsadf");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "123456789");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "zxcvasdfewr");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "REDGEM92");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "Redgem92");
-        getValidateJsonAndExpectOK("/users/validate/account","accountId", "jisuJiUS12");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "redgem92");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "12qewrsadf");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "123456789");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "zxcvasdfewr");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "REDGEM92");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "Redgem92");
+        getValidateJsonAndExpectOK("/users/validate-account","accountId", "jisuJiUS12");
     }
 
     @Test
     void whenInvalidPasswordThenReturnException() throws Exception {
-        getValidateJsonAndExpectError("/users/validate/password", "password","", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","ASDFQ", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","1!AS", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","1q2w3e4r1!((", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","한글도안됨ㅎ", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","ASㅎㅁㄴㅇㄹㄴ", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","asdfqwerasdf)-+_", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/password", "password","SDFAERGSGBSRTYRTHfdfweyro3fhefaidaosidf", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","ASDFQ", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","1!AS", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","1q2w3e4r1!((", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","한글도안됨ㅎ", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","ASㅎㅁㄴㅇㄹㄴ", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","asdfqwerasdf)-+_", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-password", "password","SDFAERGSGBSRTYRTHfdfweyro3fhefaidaosidf", ErrorCode.INVALID_INPUT_VALUE);
     }
 
     @Test
     void whenValidPasswordThenReturnOK() throws Exception {
-        getValidateJsonAndExpectOK("/users/validate/password","password", "1q2w3e4r1!");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "1234567@@#$");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "123456789");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "zxcvasdfewr");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "JIsue%$#@@#%^&");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "!@#$%^&*");
-        getValidateJsonAndExpectOK("/users/validate/password","password", "~!@#$%^&DSacvzxc");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "1q2w3e4r1!");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "1234567@@#$");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "123456789");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "zxcvasdfewr");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "JIsue%$#@@#%^&");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "!@#$%^&*");
+        getValidateJsonAndExpectOK("/users/validate-password","password", "~!@#$%^&DSacvzxc");
     }
 
     @Test
     void whenInvalidNicknameThenReturnException() throws Exception {
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","a", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","ㅎㅎㅎㅎㅎ", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","@.@", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","오빠라고불러다오!", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","김미어콜베이베~베이베~", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","kamehameha^^", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","~崔志秀~", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","Songoku's げんきぎょく", ErrorCode.INVALID_INPUT_VALUE);
-        getValidateJsonAndExpectError("/users/validate/nickname", "nickname","김미김미나 김미김미나 뚜두두두두", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","a", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","ㅎㅎㅎㅎㅎ", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","@.@", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","오빠라고불러다오!", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","김미어콜베이베~베이베~", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","kamehameha^^", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","~崔志秀~", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","Songoku's げんきぎょく", ErrorCode.INVALID_INPUT_VALUE);
+        getValidateJsonAndExpectError("/users/validate-nickname", "nickname","김미김미나 김미김미나 뚜두두두두", ErrorCode.INVALID_INPUT_VALUE);
     }
 
     @Test
     void whenValidNicknameThenReturnOK() throws Exception {
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "MX");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "MX");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "F-lab");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "jisus.choi");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "崔志秀");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "Songoku-げんきぎょく");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "_--_");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "im-so-sexy");
-        getValidateJsonAndExpectOK("/users/validate/nickname","nickname", "strong_minsu");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "MX");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "MX");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "F-lab");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "jisus.choi");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "崔志秀");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "Songoku-げんきぎょく");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "_--_");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "im-so-sexy");
+        getValidateJsonAndExpectOK("/users/validate-nickname","nickname", "strong_minsu");
     }
 
     void getValidateJsonAndExpectError(
@@ -123,12 +123,12 @@ public class UserControllerTest extends IntegrationTest {
     void withoutSignInThenChangeNicknameAndPassword() throws Exception {
         final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("password", "qwer1234");
-        getMethod("/users/change/check/password",  map,  ErrorCode.HANDLE_ACCESS_DENIED);
-        patchMethod("/users/change/password", map, ErrorCode.HANDLE_ACCESS_DENIED);
+        getMethod("/users/check-password",  map,  ErrorCode.HANDLE_ACCESS_DENIED);
+        patchMethod("/users/password", map, ErrorCode.HANDLE_ACCESS_DENIED);
         map.clear();
         map.add("nickname", "nickname");
-        getMethod("/users/change/check/password", map, ErrorCode.HANDLE_ACCESS_DENIED);
-        patchMethod("/users/change/password", map, ErrorCode.HANDLE_ACCESS_DENIED);
+        getMethod("/users/check-password", map, ErrorCode.HANDLE_ACCESS_DENIED);
+        patchMethod("/users/password", map, ErrorCode.HANDLE_ACCESS_DENIED);
     }
 
     private void getMethod(
