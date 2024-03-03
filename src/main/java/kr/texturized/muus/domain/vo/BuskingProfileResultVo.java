0f8331f3aa7448cf9ba951df2fe268d0e682d1a5
Jisu Choi <redgem92@gmail.com>
2024-03-04 01:35:38 +0900

    CHORE: 레거시 코드 삭제

--- src/main/java/kr/texturized/muus/domain/vo/BuskingProfileResultVo.java
+++ src/main/java/kr/texturized/muus/domain/vo/BuskingProfileResultVo.java
@@ -1,6 +1,5 @@
 package kr.texturized.muus.domain.vo;
 
-import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -25,18 +24,3 @@ public class BuskingProfileResultVo {
     private List<String> keywords = new ArrayList<>();
     private List<String> imagePaths = new ArrayList<>();
 }
-/*public record BuskingProfileResultVo(
-    Long userId,
-    String nickname,
-    String profileImagePath,
-    String title,
-    String description,
-    Double latitude,
-    Double longitude,
-    LocalDateTime managedStartTime,
-    LocalDateTime managedEndTime,
-    LocalDateTime endTime,
-    List<String> keywords*//*,
-    List<String> imagePaths*//*
-) {
-}*/
